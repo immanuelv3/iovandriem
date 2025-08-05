1eaf09643b7e04ad36414deb6f917b6f4d13a33c
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-05 15:24:49 +0200

    Update scriipt.js

--- scriipt.js
+++ scriipt.js
@@ -5,7 +5,7 @@ document.writeln("<a href='start.html'>Start</a>");
 document.writeln("<a href='whoiam.html'>wer ich bin</a>");
 document.writeln("<a href='music.html'>music</a>");
 document.writeln("<a href='log.html'>log</a>");
-document.writeln("<a href='brot backen.html'>brot backen</a>");
+document.writeln("<a href='brotbacken.html'>brot backen</a>");
 
 document.writeln("<a href='mysocialmedia.html'>My social media and links</a>");
 /*
