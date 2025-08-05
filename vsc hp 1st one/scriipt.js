cd5742fd9c02b7c94c55548cf5c71b7130568e41
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-05 14:45:16 +0200

    Add files via upload

---
+++ vsc hp 1st one/scriipt.js
@@ -0,0 +1,13 @@
+
+
+document.writeln("<a href='start.html'>Start</a>");
+/* document.writeln("<"); */
+document.writeln("<a href='whoiam.html'>wer ich bin</a>");
+document.writeln("<a href='music.html'>music</a>");
+document.writeln("<a href='log.html'>log</a>");
+document.writeln("<a href='brot backen.html'>brot backen</a>");
+
+document.writeln("<a href='mysocialmedia.html'>My social media and links</a>");
+/*
+document.writeln("<a href='time.php'>php</a>");
+*/
