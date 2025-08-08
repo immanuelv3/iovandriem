71f2403eaf50caec9cb3e621c868130d677420a8
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-08 11:20:44 +0200

    Add files via upload

--- vsc hp 1st one/scriipt.js
+++ vsc hp 1st one/scriipt.js
@@ -1,13 +1,71 @@
+// Start dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'start.html\'">start</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
 
+// About me dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'whoiam.html\'">about me</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">yes</a>');
+document.writeln('</div>');
+document.writeln('</div>');
 
-document.writeln("<a href='start.html'>Start</a>");
-/* document.writeln("<"); */
-document.writeln("<a href='whoiam.html'>wer ich bin</a>");
-document.writeln("<a href='music.html'>music</a>");
-document.writeln("<a href='log.html'>log</a>");
-document.writeln("<a href='brot backen.html'>brot backen</a>");
+// med dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'med.html\'">medicine</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://www.kirsten-greene.com/">kirsten greene nd</a>');
+document.writeln('</div>');
+document.writeln('</div>');
 
-document.writeln("<a href='mysocialmedia.html'>My social media and links</a>");
-/*
-document.writeln("<a href='time.php'>php</a>");
-*/
+// Music dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'music.html\'">music</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+// sm dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">my social media</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+// personal links dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">personal links</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+// baking dropdown
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">baking</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+// log
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'log.html\'">log</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
+
+// grammatik
+document.writeln('<div class="dropdown">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'grammatik.html\'">grammatik</button>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('</div>');
+document.writeln('</div>');
