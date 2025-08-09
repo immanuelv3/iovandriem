649e5c0e377fb3c54f644c4409d6fd04a0100a46
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-09 08:52:32 +0200

    Add files via upload

--- scriipt.js
+++ scriipt.js
@@ -2,7 +2,7 @@
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'start.html\'">start</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://yahoo.de">yahoo</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
@@ -10,7 +10,7 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'whoiam.html\'">about me</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">yes</a>');
+document.writeln('<a href="https://google.de">google</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
@@ -26,7 +26,7 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'music.html\'">music</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://www.stadt-freiburg.de">freiburg</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
@@ -34,23 +34,24 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">my social media</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://facebook.com/immanuel.vandriem">facebook</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
 // personal links dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">personal links</button>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'personallinks.html\'">personal links</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://www.facebook.com/immanuel.vandriem">facebook</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
 // baking dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">baking</button>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'brotbacken.html\'">baking</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://immanuelvandriem.wixsite.com/baking">bread-wix-site</a>');
+document.writeln('">sss</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
@@ -58,7 +59,7 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'log.html\'">log</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="aboutlog.html">"gg"-about log</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
@@ -66,6 +67,36 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'grammatik.html\'">grammatik</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://google.de">sss</a>');
+document.writeln('<a href="https://google.de">google</a>');
 document.writeln('</div>');
 document.writeln('</div>');
+
+
+
+// Add this to a JS file or a <script> tag
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('commentForm');
+  const input = document.getElementById('commentInput');
+  const commentsDiv = document.getElementById('comments');
+
+   // Load comments from localStorage
+  let comments = JSON.parse(localStorage.getItem('comments')) || [];
+  comments.forEach(function(comment) {
+    const p = document.createElement('p');
+    p.textContent = comment;
+    commentsDiv.appendChild(p);
+  });
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const comment = input.value.trim();
+    if (comment) {
+      comments.push(comment);
+      localStorage.setItem('comments', JSON.stringify(comments));
+      const p = document.createElement('p');
+      p.textContent = comment;
+      commentsDiv.appendChild(p);
+      input.value = '';
+    }
+  });
+});
