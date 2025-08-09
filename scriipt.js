2d0e4f279ad385215f1736bbd104106ec64e9f89
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-09 09:18:17 +0200

    Update scriipt.js
    freiburg.de

--- scriipt.js
+++ scriipt.js
@@ -26,7 +26,7 @@ document.writeln('</div>');
 document.writeln('<div class="dropdown">');
 document.writeln('<button class="dropbtn" onclick="location.href=\'music.html\'">music</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://www.stadt-freiburg.de">freiburg</a>');
+document.writeln('<a href="https://www.freiburg.de">freiburg</a>');
 document.writeln('</div>');
 document.writeln('</div>');
 
