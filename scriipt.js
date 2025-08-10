a8585d7a949c8c94745e14988ce412eaf566fd8f
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-10 17:12:41 +0200

    Add files via upload

--- scriipt.js
+++ scriipt.js
@@ -1,63 +1,85 @@
 // Start dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'start.html\'">start</button>');
-document.writeln('<div class="dropdown-content">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/index.html\'">start</button>');
+/* document.writeln('<div class="dropdown-content">');
 document.writeln('<a href="https://yahoo.de">yahoo</a>');
-document.writeln('</div>');
+document.writeln('</div>'); */
 document.writeln('</div>');
 
 // About me dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'whoiam.html\'">about me</button>');
-document.writeln('<div class="dropdown-content">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/whoiam.html\'">about me</button>');
+/*document.writeln('<div class="dropdown-content">');
 document.writeln('<a href="https://google.de">google</a>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="https://yahoo.de">yahoo</a>');
+
 document.writeln('</div>');
+document.writeln('</div>');*/
 document.writeln('</div>');
 
 // med dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'med.html\'">medicine</button>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/med/medicinestart.html\'">medicine</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://www.kirsten-greene.com/">kirsten greene nd</a>');
+document.writeln('<a href="/med/stressandcortisol.html">stress</a>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="hypo.html">hypothyroidism</a>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="/med/sibo.html">sibo</a>');
+document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="/med/sibo-hit.html">hit</a>');
+//document.writeln('<div class="dropdown-content">');
+//document.writeln('<a href="microbiome.html">microbiome</a>');
+//document.writeln('<div class="dropdown-content">');
+//document.writeln('<a href="-">-</a>');
+//document.writeln('<a href="greene.html">kirsten greene nd</a>');
+
+//document.writeln('</div>');
+//document.writeln('</div>');
+document.writeln('</div>');
+document.writeln('</div>');
+document.writeln('</div>');
+
 document.writeln('</div>');
 document.writeln('</div>');
 
 // Music dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'music.html\'">music</button>');
-document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://www.freiburg.de">freiburg</a>');
-document.writeln('</div>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/music.html\'">music</button>');
+/* document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="">freiburg</a>');
+document.writeln('</div>'); */
 document.writeln('</div>');
 
 // sm dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'mysocialmedia.html\'">my social media</button>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/mysocialmedia.html\'">my social media</button>');
 document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="">none</a>');
-document.writeln('</div>');
+/* document.writeln('<a href="">none</a>');*/
+document.writeln('</div>'); 
 document.writeln('</div>');
 
 // personal links dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'personallinks.html\'">personal links</button>');
-document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://www.facebook.com/immanuel.vandriem">facebook</a>');
-document.writeln('</div>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/personallinks.html\'">personal links</button>');
+/*document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="">-</a>');
+document.writeln('</div>');*/
 document.writeln('</div>');
 
 // baking dropdown
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'brotbacken.html\'">baking</button>');
-document.writeln('<div class="dropdown-content">');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/kulinarik/brotbacken.html\'">baking</button>');
+/*document.writeln('<div class="dropdown-content">');
 document.writeln('<a href="https://immanuelvandriem.wixsite.com/baking">bread-wix-site</a>');
 document.writeln('(on baking bread...)');
-document.writeln('</div>');
+document.writeln('</div>');*/
 document.writeln('</div>');
 
 // log
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'log.html\'">log</button>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/log.html\'">log</button>');
 document.writeln('<div class="dropdown-content">');
 document.writeln('<a href="aboutlog.html">about log</a>');
 document.writeln('</div>');
@@ -65,10 +87,10 @@ document.writeln('</div>');
 
 // grammatik
 document.writeln('<div class="dropdown">');
-document.writeln('<button class="dropbtn" onclick="location.href=\'grammatik.html\'">grammatik</button>');
-document.writeln('<div class="dropdown-content">');
-document.writeln('<a href="https://googletranslate.com">google translate</a>');
-document.writeln('</div>');
+document.writeln('<button class="dropbtn" onclick="location.href=\'/sprache/grammatik.html\'">grammatik</button>');
+/* document.writeln('<div class="dropdown-content">');
+document.writeln('<a href="-">-</a>');
+document.writeln('</div>'); */
 document.writeln('</div>');
 
 
