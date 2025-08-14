9c8d033e8f75d21068ce6a85c2e29302efc2d380
immanuelv3 <I.r.vandriem@hotmail.com>
2025-08-14 08:58:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="thestyling.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>                                                               
+<body>
+
+<div class="topnav">
+<script src="scriipt.js"></script>
+</div>
+<div style="padding: 33px;"> 
+  <br>
+    <div> 
+      <hr>
+    <h1> Welcome to my homepage! </h1>
+    <pre style="padding-left: 20px; font-style: italic; font-size: large;">- Immanuel Orfeo van Driem</pre>
+
+    <?php
+date_default_timezone_set("Europe/Amsterdam");
+echo "The time is " . date("h:i:sa - ");        
+echo "";
+echo "The date is " . date ("jS-M-y");
+?>
+<hr>
+</div>
+
+<br><br>
+  
+  <p>
+  
+</p>
+</div>
+<div style="padding-left: 14px;"> 
+</div>
+
+
+</body>
+</html>
